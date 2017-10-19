267b3308ed8ac1b22249f74a2164759197ddc4e4
iMxAndyy <Admin@mine-byte.com>
2017-10-19 11:05:38 +0100

    Task 1
    Attempt at task 1 of the week 4 workshop, still needs to be throughly
    tested to see if it connects to the server but it is a start point none
    the less

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,22 @@
 package com.company;
 
+
+import java.io.*;
+import java.net.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+	// write your code here
 
-        //code
+        Socket clientSocket = new Socket("192.168.0.108",5000);
 
+        while(true)
+        {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+        }
     }
+
 }
